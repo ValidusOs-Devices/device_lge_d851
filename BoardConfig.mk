3138fc92e903def9a58c837dbdca3322ee1bb4e0
Matthew Opperman <mattopp17@gmail.com>
2016-03-31 02:03:54 -0400

    remove tc/opt info for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,18 +27,6 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_d851_defconfig
 
-# Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
-KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
-# Rom Toolchain
-TARGET_GCC_VERSION_EXP := 4.9-uber
-# Optimizations
-CLANG_O3 := true
-STRICT_ALIASING := false
-KRAIT_TUNINGS := true
-GRAPHITE_OPTS := false
-ENABLE_GCCONLY := true
-
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216

e83e6ca6e47a7179acf2ed53855557277adffe89
oadam11 <ovadia.adam@gmail.com>
2017-02-26 22:08:49 +0100

    val pls

---
+++ validus.mk
@@ -0,0 +1,20 @@
+PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+$(call inherit-product, device/lge/d851/full_d851.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+
+$(call inherit-product, vendor/validus/config/caf_fw.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
+
+PRODUCT_NAME := validus_d851
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Adam Ovadia (oadam11)" \
+    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_tmo_us" \
+    BUILD_FINGERPRINT="lge/g3_tmo_us/g3:6.0/MRA58K/1609517159bca:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_tmo_us-user 6.0 MRA58K 1609517159bca release-keys"

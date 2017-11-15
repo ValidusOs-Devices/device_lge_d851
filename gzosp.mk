b05688c6fce17acd5f1eaff12f765d84ceea4f70
oadam11 <ovadia.adam@gmail.com>
2017-11-15 08:21:54 -0500

    gzosp-ify

--- gzosp.mk
+++ gzosp.mk
@@ -3,9 +3,9 @@ PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 $(call inherit-product, device/lge/d851/full_d851.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_d851
+PRODUCT_NAME := gzosp_d851
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
